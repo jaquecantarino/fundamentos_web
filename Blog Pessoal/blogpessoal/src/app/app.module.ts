4e3e318a149ba2f60dac5b19aae3dfd73943b458
jaquecantarino <jaquelinecantarino@outlook.com>
2021-09-02 11:24:55 -0300

    Implementando inicio

--- Blog Pessoal/blogpessoal/src/app/app.module.ts
+++ Blog Pessoal/blogpessoal/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { FormsModule } from '@angular/forms';
 
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -28,7 +29,10 @@ import { InicioComponent } from './inicio/inicio.component';
     HttpClientModule,
     FormsModule
   ],
-  providers: [],
+  providers: [{
+    provide: LocationStrategy,
+    useClass: HashLocationStrategy
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

aff21c62fb5a172e03203985022552d503274ec6
jaquecantarino <jaquelinecantarino@outlook.com>
2021-09-02 15:47:53 -0300

    consumo de crud de tema

--- Blog Pessoal/blogpessoal/src/app/service/tema.service.ts
+++ Blog Pessoal/blogpessoal/src/app/service/tema.service.ts
@@ -21,8 +21,21 @@ export class TemaService {
     return this.http.get<Tema[]>('https://jaqueblog.herokuapp.com/temas', this.token)
   }
 
+  getByIdTema(id: number): Observable<Tema>{
+    return this.http.get<Tema>(`https://jaqueblog.herokuapp.com/temas/${id}`, this.token)
+
+  }
+
   postTema(tema: Tema): Observable<Tema>{
     return this.http.post<Tema>('https://jaqueblog.herokuapp.com/temas',tema, this.token)
+  }
+
+  putTema(tema: Tema): Observable<Tema>{
+    return this.http.put<Tema>('https://jaqueblog.herokuapp.com/temas', tema, this.token)
+  }
+
+  deleteTema(id: number){
+    return this.http.delete(`https://jaqueblog.herokuapp.com/temas/${id}`, this.token)
   }
 
 }
